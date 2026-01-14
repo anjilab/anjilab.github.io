054f4384075c82e104b0b2249cb27bbe384650b8
anjilab <anzilabudathoki@gmail.com>
2026-01-14 17:01:46 +0000

    Deploying to gh-pages from @ anjilab/anjilab.github.io@9ed484df7669c9afb10ddef0a0e637e01a91e521 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -15,13 +15,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
